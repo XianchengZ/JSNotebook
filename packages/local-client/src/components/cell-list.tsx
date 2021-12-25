54ea83d6d6ac9db25d827c784a1c3c69850e66a8
Andrew Zang <andrewz020529@gmail.com>
2021-12-25 16:02:11 -0500

    reorganized agian

---
+++ packages/local-client/src/components/cell-list.tsx
@@ -0,0 +1,29 @@
+import './cell-list.css';
+import React, { Fragment } from 'react';
+import { useTypeSelector } from '../hooks/use-typed-selector';
+import CellListItem from './cell-list-item';
+import AddCell from './add-cell';
+
+const CellList: React.FC = () => {
+  const cells = useTypeSelector(({ cells: { order, data } }) => {
+    return order.map((id) => {
+      return data[id];
+    });
+  });
+
+  const renderedCells = cells.map((cell) => (
+    <Fragment key={cell.id}>
+      <CellListItem key={cell.id} cell={cell} />
+      <AddCell previousCellId={cell.id} />
+    </Fragment>
+  ));
+
+  return (
+    <div className="cell-list">
+      <AddCell forceVisible={cells.length === 0} previousCellId={null} />
+      {renderedCells}
+    </div>
+  );
+};
+
+export default CellList;
---
+++ packages/local-client/src/components/code-editor.tsx
@@ -0,0 +1,70 @@
+import { useRef } from 'react';
+import MonacoEditor, { EditorDidMount } from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
+import './code-editor.css';
+
+interface CodeEditorProps {
+  initialValue: string;
+  onChange(value: string): void;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ onChange, initialValue }) => {
+  const editorRef = useRef<any>();
+
+  const onEditorDidMount: EditorDidMount = (getValue, MonacoEditor) => {
+    editorRef.current = MonacoEditor;
+    MonacoEditor.onDidChangeModelContent(() => {
+      onChange(getValue());
+    });
+
+    MonacoEditor.getModel()?.updateOptions({ tabSize: 2 });
+  };
+
+  const onFormatClick = () => {
+    // get curret value from editor
+    const unformmated = editorRef.current.getModel().getValue();
+    // format the value
+    const formatted = prettier
+      .format(unformmated, {
+        parser: 'babel',
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+      })
+      .replace(/\n$/, '');
+    // set the formatted value back in the editor
+    editorRef.current.setValue(formatted);
+  };
+
+  return (
+    <div className="editor-wrapper">
+      <button
+        className="button button-format is-primary is-small"
+        onClick={onFormatClick}
+      >
+        Format
+      </button>
+      <MonacoEditor
+        editorDidMount={onEditorDidMount}
+        value={initialValue}
+        theme="dark"
+        language="javascript"
+        height="100%"
+        options={{
+          wordWrap: 'on',
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+        }}
+      />
+    </div>
+  );
+};
+
+export default CodeEditor;
